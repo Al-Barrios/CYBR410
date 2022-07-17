b5f62fb23618c16711dcd658a9851e363527cdf5
Al-Barrios <107366926+Al-Barrios@users.noreply.github.com>
2022-07-17 15:28:03 -0500

    Add files via upload

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,1 +1,29 @@
 
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.cq1azkb.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+students = db.students
+
+student_list = students.find({})
+
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Barrios"}})
+
+
+Al = students.find_one({"student_id": "1007"})
+
+print("\n  -- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+print("  Student ID: " + Al["student_id"] + "\n  First Name: " + Al["first_name"] + "\n  Last Name: " + Al["last_name"] + "\n")
+
+input("\n\n  End of program, press any key to continue...")
